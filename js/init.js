4af14763a942248e25e026a9bc4f1c840dffd030
bassplayer7 <jesse@swiftotter.com>
2016-12-28 20:13:03 -0600

    Working on adding service worker

--- js/init.js
+++ js/init.js
@@ -12,6 +12,16 @@ require.config({
     }
 });
 
-requirejs(['knockout', 'modules/game'], function(ko, Game) {
+requirejs(['knockout', 'modules/game', 'modules/services/worker'], function(ko, Game) {
     ko.applyBindings(new Game);
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/js/modules/services/sw.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
